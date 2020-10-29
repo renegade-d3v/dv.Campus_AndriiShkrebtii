2cc5765f3fabb715566dc33e9adcfd42ea1662af
Andrii S <jimmy.wilson11@gmail.com>
2020-10-29 11:25:03 +0200

    #7: Configure development server: Add Styles file

--- app/etc/env.prod.php
+++ app/etc/env.prod.php
@@ -91,8 +91,8 @@ return [
                     'base_link_url' => '{{secure_base_url}}',
                     'base_static_url' => 'https://andrii-shkrebtii.local/static/',
                     'base_media_url' => 'https://andrii-shkrebtii.local/media/'
-                ],
-            ],
+                ]
+            ]
         ],
         'websites' => [
             'secondary_site' => [
