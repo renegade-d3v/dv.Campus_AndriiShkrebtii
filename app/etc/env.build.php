0f97bb8e1d68f96e439899f8246b1a4b3c7523a3
Andrii S <jimmy.wilson11@gmail.com>
2021-01-23 15:28:07 +0200

    #39: Magento Cookies, Session, Session storage: Implement Redis for all environments

--- app/etc/env.build.php
+++ app/etc/env.build.php
@@ -35,7 +35,27 @@ return [
     'x-frame-options' => 'SAMEORIGIN',
     'MAGE_MODE' => 'production',
     'session' => [
-        'save' => 'files'
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '4',
+            'max_concurrency' => '6',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000'
+        ]
     ],
     'cache' => [
         'frontend' => [
