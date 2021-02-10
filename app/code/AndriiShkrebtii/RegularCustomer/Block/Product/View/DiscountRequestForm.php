c206ca5fe32d476c8f8d11a35ca16407257a3d2a
Andrii S <jimmy.wilson11@gmail.com>
2021-02-10 22:06:11 +0200

    #55: Magento Caching. Full Page Cache (FPC): UPD prew commit, add  file Block/DiscountRequestForm

---
+++ app/code/AndriiShkrebtii/RegularCustomer/Block/Product/View/DiscountRequestForm.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AndriiShkrebtii\RegularCustomer\Block\Product\View;
+
+class DiscountRequestForm extends \Magento\Catalog\Block\Product\View
+{
+    /**
+     *
+     */
+    protected function _construct(): void
+    {
+        parent::_construct();
+
+        $this->addData(
+            [
+                'cache_lifetime' => 86400,
+                'cache_tags' => [
+                    \Magento\Catalog\Model\Product::CACHE_TAG
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getCacheKeyInfo(): array
+    {
+        return array_merge(parent::getCacheKeyInfo(), ['product_id' => $this->getProduct()->getId()]);
+    }
+}
